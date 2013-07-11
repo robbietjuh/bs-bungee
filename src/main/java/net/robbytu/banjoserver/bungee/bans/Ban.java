11d938b5f523f7f8d502488652796a6527cfa43f
Robert de Vries <r.devries@robbytu.net>
2013-07-11 19:20:33 +0200

    Implemented tempban timings. Part of BS-5.

--- src/main/java/net/robbytu/banjoserver/bungee/bans/Ban.java
+++ src/main/java/net/robbytu/banjoserver/bungee/bans/Ban.java
@@ -10,16 +10,22 @@ public class Ban {
     public String reason;
     public int date;
     public String server;
+    public int tempban;
     public boolean active;
 
     public Ban() {
         this.id = 0;
         this.date = (int) (System.currentTimeMillis() / 1000L);
         this.active = true;
+        this.tempban = 0;
     }
 
     public void setActive(int active) {
         this.active = (active == 1);
+    }
+
+    public boolean isTempban() {
+        return (this.tempban > 0);
     }
 
     public String getFriendlyDate() {
