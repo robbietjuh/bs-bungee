a356f3db19f2b2ed1d11771c9c7396d0429c2064
Robert de Vries <r.devries@robbytu.net>
2013-07-11 19:36:41 +0200

    Implemented getUserBans

--- src/main/java/net/robbytu/banjoserver/bungee/bans/Bans.java
+++ src/main/java/net/robbytu/banjoserver/bungee/bans/Bans.java
@@ -3,14 +3,17 @@ package net.robbytu.banjoserver.bungee.bans;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.robbytu.banjoserver.bungee.Main;
+import net.robbytu.banjoserver.bungee.warns.Warn;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Bans {
     public static void banUser(Ban ban) {
-        // Insert User warn
+        // Insert User ban
         try {
             Connection conn = Main.conn;
             PreparedStatement statement = conn.prepareStatement("INSERT INTO bs_bans (user, mod, reason, date, server, tempban, active) VALUES (?, ?, ?, ?, ?, ?, 1)");
@@ -38,6 +41,43 @@ public class Bans {
         catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Ban[] getUserBans(String username) {
+        Connection conn = Main.conn;
+        ArrayList<Ban> bans = new ArrayList<Ban>();
+
+        try {
+            // Create a new select statement
+            PreparedStatement statement = conn.prepareStatement("SELECT id, user, mod, reason, date, server, tempban, active FROM bs_warns WHERE username LIKE ?");
+            statement.setString(1, username);
+            ResultSet result = statement.executeQuery();
+
+            // For each ban ...
+            while(result.next()) {
+                // Create a new Ban instance
+                Ban ban = new Ban();
+
+                // Fill in properties
+                ban.id = result.getInt(1);
+                ban.username = result.getString(2);
+                ban.mod = result.getString(3);
+                ban.reason = result.getString(4);
+                ban.date = result.getInt(5);
+                ban.server = result.getString(6);
+                ban.tempban = result.getInt(7);
+                ban.setActive(result.getInt(8));
+
+                // Add ban to return array
+                bans.add(ban);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Return the array of bans
+        return bans.toArray(new Ban[bans.size()]);
     }
 
     public static void kickBannedPlayer(Ban ban) {
