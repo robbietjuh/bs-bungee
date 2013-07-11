00e0ffe019ccfec2349331c3b142082cac329c98
Robert de Vries <r.devries@robbytu.net>
2013-07-12 01:53:51 +0200

    Fixes

--- src/main/java/net/robbytu/banjoserver/bungee/listeners/LoginListener.java
+++ src/main/java/net/robbytu/banjoserver/bungee/listeners/LoginListener.java
@@ -1,13 +1,13 @@
 package net.robbytu.banjoserver.bungee.listeners;
 
-import com.google.common.eventbus.Subscribe;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 
+import net.md_5.bungee.event.EventHandler;
 import net.robbytu.banjoserver.bungee.bans.BanLoginListener;
 
 public class LoginListener implements Listener {
-    @Subscribe
+    @EventHandler
     public static void login(LoginEvent event) {
         if(!event.isCancelled()) BanLoginListener.handle(event);
     }
