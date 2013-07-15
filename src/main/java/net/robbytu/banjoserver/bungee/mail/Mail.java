16939a0da524eeb1924f5dfcd1dfa4282a8862fd
Robert de Vries <r.devries@robbytu.net>
2013-07-15 05:08:59 +0200

    Updated Mail model

--- src/main/java/net/robbytu/banjoserver/bungee/mail/Mail.java
+++ src/main/java/net/robbytu/banjoserver/bungee/mail/Mail.java
@@ -1,11 +1,12 @@
 package net.robbytu.banjoserver.bungee.mail;
 
 public class Mail {
-    String from_user = "";
-    String to_user = "";
-    int date = (int) (System.currentTimeMillis() / 1000L);
-    String message = "";
-    boolean unread = true;
+    public int id = 0;
+    public String from_user = "";
+    public String to_user = "";
+    public int date = (int) (System.currentTimeMillis() / 1000L);
+    public String message = "";
+    public boolean unread = true;
 
     public void setUnread(int unread) {
         this.unread = (unread == 1);
