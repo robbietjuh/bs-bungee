c6f5932be5e77e20853675a0bc245c1b7ee913af
Robert de Vries <r.devries@robbytu.net>
2013-07-17 14:10:13 +0200

    Implemented Donation model

---
+++ src/main/java/net/robbytu/banjoserver/bungee/donate/Donation.java
@@ -0,0 +1,10 @@
+package net.robbytu.banjoserver.bungee.donate;
+
+public class Donation {
+    public int id = 0;
+    public String title = "";
+    public String description = "";
+    public String code = "";
+    public String server = "";
+    public String tag = "";
+}
