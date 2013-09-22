024b7a22f8de7a88aa44d0f16d04216fd2c03031
Robert de Vries <r.devries@robbytu.net>
2013-09-22 21:15:37 +0200

    Implemented authentication

--- src/main/java/net/robbytu/banjoserver/bungee/auth/AuthProvider.java
+++ src/main/java/net/robbytu/banjoserver/bungee/auth/AuthProvider.java
@@ -2,8 +2,8 @@ package net.robbytu.banjoserver.bungee.auth;
 
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.robbytu.banjoserver.bungee.Main;
-import net.robbytu.banjoserver.bungee.directsupport.Ticket;
 
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,11 +18,35 @@ public class AuthProvider {
     }
 
     public static boolean authenticate(ProxiedPlayer player, String providedPassword) {
-        // Todo
+        Connection conn = Main.conn;
+
+        try {
+            PreparedStatement statement = conn.prepareStatement("SELECT password FROM bs_auth WHERE username = ?");
+            statement.setString(1, player.getName());
+            ResultSet result = statement.executeQuery();
+
+            if(result.next()) {
+                String password = result.getString(0);
+
+                String salt = password.split("$")[2];
+
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                md.update((providedPassword + salt).getBytes("UTF-8"));
+                byte[] digest = md.digest();
+
+                String hashed_password = "$SHA$" + salt + "$" + new String(digest);
+                if(password == hashed_password) {
+                    authenticatedUsers.add(player);
+                    return true;
+                }
+            }
+        }
+        catch(Exception ignored) {}
+
         return false;
     }
 
-    public static void unauthorizePlayer(ProxiedPlayer player) {
+    public static void unauthenticatePlayer(ProxiedPlayer player) {
         authenticatedUsers.remove(player);
     }
 
