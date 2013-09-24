bd6f4005ce00e6298fb23f279a9a34532a247c14
Robert de Vries <r.devries@robbytu.net>
2013-09-24 23:14:25 +0200

    Implemented register functionality into AuthProvider. Part of BS-69, BS-74.

--- src/main/java/net/robbytu/banjoserver/bungee/auth/AuthProvider.java
+++ src/main/java/net/robbytu/banjoserver/bungee/auth/AuthProvider.java
@@ -6,6 +6,7 @@ import net.robbytu.banjoserver.bungee.Main;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AuthProvider {
+    private static SecureRandom rnd = new SecureRandom();
     private static List<ProxiedPlayer> authenticatedUsers = new ArrayList<ProxiedPlayer>();
 
     public static boolean isAuthenticated(ProxiedPlayer player) {
@@ -37,6 +39,26 @@ public class AuthProvider {
                     return true;
                 }
             }
+        }
+        catch(Exception ignored) {}
+
+        return false;
+    }
+
+    public static boolean register(ProxiedPlayer player, String password) {
+        Connection conn = Main.conn;
+
+        try {
+            String salt = createSalt(16);
+            String hashed_password = "$SHA$" + salt + "$" + getSHA256(getSHA256(password) + salt);
+
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO bs_auth (username, password, ip) VALUES (?, ?, ?)");
+            statement.setString(1, player.getName());
+            statement.setString(2, hashed_password);
+            statement.setString(3, player.getAddress().getAddress().toString());
+            statement.executeUpdate();
+
+            return true;
         }
         catch(Exception ignored) {}
 
@@ -71,5 +93,15 @@ public class AuthProvider {
         byte[] digest = sha256.digest();
 
         return String.format("%0" + (digest.length << 1) + "x", new Object[] { new BigInteger(1, digest) });
+    }
+
+    private static String createSalt(int length) throws NoSuchAlgorithmException {
+        byte[] msg = new byte[40];
+        rnd.nextBytes(msg);
+
+        MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+        sha1.reset();
+        byte[] digest = sha1.digest(msg);
+        return String.format("%0" + (digest.length << 1) + "x", new Object[] { new BigInteger(1, digest) }).substring(0, length);
     }
 }
