8fc392853aca309f5b0154c4c75ae620196d702b
Robert de Vries <r.devries@robbytu.net>
2013-09-28 20:47:27 +0200

    Implemented dispatch event into PluginMessageListener. Part of BS-12.

--- src/main/java/net/robbytu/banjoserver/bungee/listeners/PluginMessageListener.java
+++ src/main/java/net/robbytu/banjoserver/bungee/listeners/PluginMessageListener.java
@@ -1,6 +1,24 @@
 package net.robbytu.banjoserver.bungee.listeners;
 
+import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.robbytu.banjoserver.bungee.Main;
+import net.robbytu.banjoserver.bungee.consoles.ConsoleGateway;
 
 public class PluginMessageListener implements Listener {
+    @EventHandler
+    public static void messageReceived(PluginMessageEvent event) {
+        if(event.getTag().equals("LogGateway")) {
+            String data = new String(event.getData());
+
+            String server = data.split(":", 1)[0];
+            String log = data.split(":", 1)[1];
+
+            ConsoleGateway.dispatchLog(server, log);
+        }
+        else {
+            Main.instance.getLogger().warning("Received incompatible bs-framework plugin command with tag " + event.getTag());
+        }
+    }
 }
