026a907fbcaba45eada34749689d5296bb467b90
Robert de Vries <r.devries@robbytu.net>
2014-10-21 23:24:00 +0200

    Sync

--- src/main/java/net/robbytu/banjoserver/bungee/listeners/PluginMessageListener.java
+++ src/main/java/net/robbytu/banjoserver/bungee/listeners/PluginMessageListener.java
@@ -9,15 +9,35 @@ import net.robbytu.banjoserver.bungee.consoles.ConsoleGateway;
 public class PluginMessageListener implements Listener {
     @EventHandler
     public static void messageReceived(PluginMessageEvent event) {
-        if(event.getTag().equals("LogGateway")) {
-            String data = new String(event.getData());
+        String orig_data = new String(event.getData());
 
+        Main.instance.getLogger().warning(orig_data);
+
+        String data_split[] = orig_data.split("/", 2);
+
+        String tag = data_split[0];
+        String data = data_split[1];
+
+        if(tag.equals("LogGateway")) {
             String server = data.split(":", 1)[0];
             String log = data.split(":", 1)[1];
 
             ConsoleGateway.dispatchLog(server, log);
         }
+        else if(tag.equals("ClearInventoryGlobal")) {
+            String admin = data.split("/")[0];
+            String user = data.split("/")[1];
+
+
+            Main.instance.getLogger().warning(data);
+        }
         else {
+            data = new String(event.getData());
+
+            Main.instance.getLogger().warning(data);
+
+
+
             Main.instance.getLogger().warning("Received incompatible bs-framework plugin command with tag " + event.getTag());
         }
     }
