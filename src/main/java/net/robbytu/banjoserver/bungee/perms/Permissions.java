b55dcbbfd621a48ca749d0a1b8f370641a16971e
Robert de Vries <r.devries@robbytu.net>
2014-10-27 21:22:46 +0100

    + Basic Permissions implementation

--- src/main/java/net/robbytu/banjoserver/bungee/perms/Permissions.java
+++ src/main/java/net/robbytu/banjoserver/bungee/perms/Permissions.java
@@ -1,13 +1,11 @@
 package net.robbytu.banjoserver.bungee.perms;/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 import net.robbytu.banjoserver.bungee.Main;
-import net.robbytu.banjoserver.bungee.mail.Mail;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class Permissions {
     public static String getPrefixForUser(String user) {
@@ -15,7 +13,7 @@ public class Permissions {
 
         try {
             // Create a new select statement
-            PreparedStatement statement = conn.prepareStatement("SELECT `bs_perm_groups`.`prefix` AS `group_name` " +
+            PreparedStatement statement = conn.prepareStatement("SELECT `bs_perm_groups`.`prefix` " +
                                                                 "FROM `bs_perm_groups` " +
                                                                 "INNER JOIN `bs_perm_members` ON `bs_perm_members`.`group` = `bs_perm_groups`.`id`" +
                                                                 "WHERE bs_perm_members.user LIKE ? " +
@@ -31,5 +29,54 @@ public class Permissions {
         }
 
         return "§3Speler";
+    }
+
+    public static boolean hasPermission(String user, String permission) {
+        return (Permissions.getPermissionEntry(user, permission) == null) ? false : true;
+    }
+
+    public static String[] getPermissionEntry(String user, String permission) {
+        return Permissions.getPermissionEntry(user, permission, "bungeecord", "bungeecord");
+    }
+
+    public static String[] getPermissionEntry(String user, String permission, String server, String world) {
+        Connection conn = Main.conn;
+
+        try {
+            // Create a new select statement
+            PreparedStatement statement = conn.prepareStatement(
+                    "SELECT `bs_perm_perms`.`permission` AS `permission`,\n" +
+                    "       `bs_perm_perms`.`options` AS `options`,\n" +
+                    "       `bs_perm_groups`.`name` AS `group_name`\n" +
+
+                    "FROM `bs_perm_perms`\n" +
+
+                    "INNER JOIN `bs_perm_groups` ON `bs_perm_perms`.`group` = `bs_perm_groups`.`id`\n" +
+                    "INNER JOIN `bs_perm_members` ON `bs_perm_perms`.`group` = `bs_perm_members`.`group`\n" +
+
+                    "WHERE `bs_perm_members`.`user` IN (?, 'default')\n" +
+                    "  AND ? REGEXP `bs_perm_members`.`server`\n" +
+                    "  AND ? REGEXP `bs_perm_perms`.`world`\n" +
+                    "  AND ? REGEXP `bs_perm_perms`.`permission`\n" +
+
+                    "ORDER BY `bs_perm_groups`.`id` DESC\n" +
+                    "LIMIT 1;");
+
+            statement.setString(1, user);
+            statement.setString(2, server);
+            statement.setString(3, world);
+            statement.setString(4, permission);
+
+            ResultSet result = statement.executeQuery();
+
+            if(result.next()) return new String[] { result.getString(1),
+                                                    result.getString(2),
+                                                    result.getString(3) };
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
